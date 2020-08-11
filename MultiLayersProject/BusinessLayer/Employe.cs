a6982d5fb49c63cb6a5e00b1373a60862115e4d1
Martin Ernesto Castillo Gonzalez <made.in.chiapas@gmail.com>
2020-08-11 13:41:12 -0500

    Publishing project files

---
+++ MultiLayersProject/BusinessLayer/Employe.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class Employe
+    {
+        public static List<DataLayer.Empleado> Get()
+        {
+            List<DataLayer.Empleado> list = new List<DataLayer.Empleado>();
+
+            using (DataLayer.BanorteBDEntities db = new DataLayer.BanorteBDEntities())
+            {
+                return db.Empleado.ToList();
+            }
+
+            return list;
+        }
+    }
+}
---
+++ MultiLayersProject/ServiceLayer/Service.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer
+{
+    
+    public class Service
+    {
+        private static string  url = "https://jsonplaceholder.typicode.com/posts";
+        public static string GetPost()
+        {
+            WebRequest request = WebRequest.Create(url);
+            request.Method = "GET";
+            var response = (HttpWebResponse)request.GetResponse();
+            using (var stream = new StreamReader(response.GetResponseStream()))
+            {
+                return stream.ReadToEnd();
+
+            }
+        }
+    }
+}
